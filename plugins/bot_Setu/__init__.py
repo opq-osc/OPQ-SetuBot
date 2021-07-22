7003de02af746ecb250f1cfc23069e0c1ab350b9
yuban10703 <2846021566@qq.com>
2021-07-22 23:50:00 +0800

    尝试修复内存占用过多

--- plugins/bot_Setu/__init__.py
+++ plugins/bot_Setu/__init__.py
@@ -1,20 +1,23 @@
 from botoy import FriendMsg, GroupMsg, S
 from botoy import decorators as deco
-from loguru import logger
 from .setu import Setu
 from .model import GetSetuConfig
+from typing import Union
 import re
+import gc
 
-setuPattern = '来(.*?)[点丶、个份张幅](.*?)的?(|[rR]18)[色瑟涩䔼😍🐍][图圖🤮]'
 __doc__ = """色图姬"""
 
+setuPattern = '来(.*?)[点丶、个份张幅](.*?)的?([rR]18)?[色瑟涩䔼😍🐍][图圖🤮]'
+digitalConversionDict = {'一': 1, '二': 2, '两': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9,
+                         '十': 10}
+# gc.set_debug(gc.DEBUG_LEAK)
 
-def check_and_processing(ctx: [GroupMsg, FriendMsg]) -> GetSetuConfig:
+
+def check_and_processing(ctx: Union[GroupMsg, FriendMsg]) -> Union[GetSetuConfig, None]:
     send = S.bind(ctx)
     info = ctx._match
     config = GetSetuConfig()
-    digitalConversionDict = {'一': 1, '二': 2, '两': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9,
-                             '十': 10}
     if info[1] != '':
         if info[1] in digitalConversionDict.keys():
             config.toGetNum = int(digitalConversionDict[info[1]])
@@ -24,26 +27,30 @@ def check_and_processing(ctx: [GroupMsg, FriendMsg]) -> GetSetuConfig:
             else:
                 send.text('能不能用阿拉伯数字?')
                 # logger.info('非数字')
-                return
+                return None
     else:  # 未指定数量,默认1
         config.toGetNum = 1
     config.tags = [i for i in list(set(re.split(r'[,， ]', info[2]))) if i != '']
-    if info[3] != '':  # r18关键字
+    if info[3]:  # r18关键字
         config.level = 1
     return config
-
 
 @deco.on_regexp(setuPattern)
 @deco.ignore_botself
 @deco.queued_up
 def receive_group_msg(ctx: GroupMsg):
     if config := check_and_processing(ctx):
-        Setu(ctx, config).main()
-
+        setu = Setu(ctx, config)
+        setu.main()
+        del setu
+        gc.collect()
 
 @deco.on_regexp(setuPattern)
 @deco.ignore_botself
 @deco.queued_up
 def receive_friend_msg(ctx: FriendMsg):
     if config := check_and_processing(ctx):
-        Setu(ctx, config).main()
+        setu = Setu(ctx, config)
+        setu.main()
+        del setu
+        gc.collect()
