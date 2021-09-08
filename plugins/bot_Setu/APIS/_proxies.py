033011da323a465b4633e6340103b8d65ac16c2c
yuban10703 <2846021566@qq.com>
2021-09-08 13:44:47 +0800

    proxies use AsyncProxyTransport

--- plugins/bot_Setu/APIS/_proxies.py
+++ plugins/bot_Setu/APIS/_proxies.py
@@ -1,8 +1,8 @@
 from botoy import jconfig
-from httpx_socks import SyncProxyTransport
+from httpx_socks import AsyncProxyTransport
 
 if proxies_socks := jconfig.proxies_socks:
-    transport = SyncProxyTransport.from_url(proxies_socks)
+    transport = AsyncProxyTransport.from_url(proxies_socks)
     proxies = None
 else:
     transport = None
