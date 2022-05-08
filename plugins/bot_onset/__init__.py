59a644f4ace90cadd02d3b935343d366f5470262
yuban10703 <yuban10703@foxmail.com>
2022-05-08 14:13:07 +0800

    发病插件屏蔽[ATALL()]

--- plugins/bot_onset/__init__.py
+++ plugins/bot_onset/__init__.py
@@ -1,8 +1,9 @@
-from pathlib import Path
-from botoy import MsgTypes, S, GroupMsg, FriendMsg
 import random
+from pathlib import Path
 from typing import Union
+
 import ujson as json
+from botoy import FriendMsg, GroupMsg, MsgTypes, S
 from botoy import async_decorators as deco
 
 curFileDir = Path(__file__).parent  # 当前文件路径
@@ -16,7 +17,7 @@ with open(curFileDir / "onset.json", "r", encoding="utf-8") as f:
 @deco.startswith("发病")
 async def main(ctx: Union[GroupMsg, FriendMsg]):
     name = ctx.Content[2:].strip()
-    if name.isspace() or len(name) == 0:
+    if name.isspace() or len(name) == 0 or "[ATALL()]" in name:
         S.text("要对谁发病捏?")
         return
     content: str = random.choice(data)["content"]
