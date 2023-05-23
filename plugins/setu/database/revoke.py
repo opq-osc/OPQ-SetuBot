6c15087b94674493e2480ad4f153923a70a981bc
Yuban10703 <yuban10703@foxmail.com>
2023-05-23 22:05:24 +0800

    fix

--- plugins/setu/database/revoke.py
+++ plugins/setu/database/revoke.py
@@ -1,5 +1,3 @@
-import time
-
 from botoy import contrib, logger
 from tinydb import where
 from tinyrecord import transaction
@@ -8,7 +6,7 @@ from ._shared import revokeTable
 
 
 @contrib.to_async
-def saveMsgSeq(group: int, msgseq: int, revoke_time: int):
+def saveMsgSeq(botqq: int, group: int, msgseq: int, revoke_time: int):
     if revoke_time == 0:
         logger.info(f"群{group}未开启撤回")
         return
@@ -17,19 +15,21 @@ def saveMsgSeq(group: int, msgseq: int, revoke_time: int):
             tr.insert(
                 {
                     "group": group,
+                    "botqq": botqq,
                     "msgseq": msgseq,
                     "revoketime": revoke_time
 
                 }
             )
-            logger.info(f"群{group} msgseq:{msgseq} 撤回时间:{revoke_time}")
+            logger.info(f"bot:{botqq} 群{group} msgseq:{msgseq} 撤回时间:{revoke_time}")
 
 
 @contrib.to_async
-def getRevokeTime(group: int, msgseq: int):
+def getRevokeTime(botqq: int, group: int, msgseq: int):
     with transaction(revokeTable) as tr:
         with tr.lock:
-            if data := revokeTable.get((where("group") == group) & (where("msgseq") == msgseq)):  # 如果有数据
+            if data := revokeTable.get(
+                    (where("group") == group) & (where("botqq") == botqq) & (where("msgseq") == msgseq)):  # 如果有数据
                 revokeTable.remove(doc_ids=[data.doc_id])
                 return data["revoketime"]
             else:
