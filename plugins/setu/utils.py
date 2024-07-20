6cb04d8daf124585b2c6f95d5f6452ac55f4621b
HeiLAAS <40287711+HeiLAAS@users.noreply.github.com>
2024-07-21 02:54:12 +0800

    修复下载函数失败时，重试无效的错误

--- plugins/setu/utils.py
+++ plugins/setu/utils.py
@@ -17,19 +17,22 @@ def transpose_setu(setu: bytes) -> bytes:
         return bf.getvalue()
 
 
-@retry(
-    stop=stop_after_attempt(3),
-    wait=wait_random(min=1, max=2),
-    retry_error_callback=lambda retry_state: "https://cdn.jsdelivr.net/gh/yuban10703/BlogImgdata/img/error.jpg",
-)
 async def download_setu(client, url) -> Union[bytes, str]:
-    res = await client.get(url)
-    if res.status_code != 200:
-        logger.warning("download_setu: res.status_code != 200")
-        raise Exception("download_setu: res.status_code != 200")
-    with BytesIO() as bf:
-        bf.write(res.content)
-        if setu_config.get("transpose"):
-            return transpose_setu(bf.getvalue())
-        bf.write(str(time.time()).encode("utf-8"))  # 增加信息,改变MD5
-        return bf.getvalue()
+    async for attempt in AsyncRetrying(stop=stop_after_attempt(3), wait=wait_random(min=1, max=2)):
+        with attempt:
+            try:
+                res = await client.get(url)
+                if res.status_code != 200:
+                    logger.warning("download_setu: res.status_code != 200")
+                    raise Exception("download_setu: res.status_code != 200")
+                with BytesIO() as bf:
+                    bf.write(res.content)
+                    if setu_config.get("transpose"):
+                        return transpose_setu(bf.getvalue())
+                    bf.write(str(time.time()).encode("utf-8"))  # 增加信息,改变MD5
+                    return bf.getvalue()
+            except Exception as e:
+                logger.error(f"Attempt failed: {e}")
+                raise  # Reraise exception to trigger retry
+
+    return "https://cdn.jsdelivr.net/gh/yuban10703/BlogImgdata/img/error.jpg"
